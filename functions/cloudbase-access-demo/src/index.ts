1a23d97417a101059ed307dd1a17966b4a657fe2
hubery.wang <hbrwang@outlook.com>
2020-10-31 14:06:59 +0800

    baseHeaders

--- functions/cloudbase-access-demo/src/index.ts
+++ functions/cloudbase-access-demo/src/index.ts
@@ -1,9 +1,17 @@
-import { Router } from "@hbrwang/cloudbase-access";
+import { HttpResult, Router } from "@hbrwang/cloudbase-access";
 import Auth from "./lib/Auth";
 
 export const main = async (event: any) => {
   console.log("event", event);
+  setHeaders();
 
   const router = new Router(event, new Auth());
-  return await router.do();
+  return (await router.do()).result;
 };
+
+function setHeaders(): void {
+  const config = <Record<string, unknown>>require("./package.json");
+  HttpResult.baseHeaders.version = config.version;
+
+  HttpResult.baseHeaders.demo = "cloudbase-access-demo";
+}
